4c06799a57d40ee832ae5d5e30c3996a968bd342
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-05-16 09:17:45 -0500

    .

---
+++ FBTutorial/FBTutorial/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FBTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBTutorial
+{
+    public class ApplicationDbContext:DbContext
+    {
+        DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
